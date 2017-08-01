07f3923a6bab1bf206ce727f74b4b6ec56a40c94
takayuki tanaka <takayuki.1981.0317@gmail.com>
2017-08-01 15:05:06 +0900

    [add] libcリンク設定追加.
    qsortをコールできるよう、libcリンクを追加.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
 extern crate rand;
+extern crate libc;
 
 use rand::Rng;
 use std::time::{Instant};
+use std::cmp::Ordering;
+use libc::{size_t, c_void, c_int};
 
 mod sort;
+
+// libcライブラリをリンク.
+#[link(name = "c")]
+extern {
+    fn qsort(data: *mut c_void,
+             num: size_t,
+             size: size_t,
+             comp: extern fn(*const c_void, *const c_void) -> c_int);
+}
+
+// qsortコールバック関数.
+extern fn _compare<T: Ord>(a: *const T, b: *const T) -> c_int {
+    match unsafe { (*a).cmp(&*b) } {
+        Ordering::Less => -1,
+        Ordering::Greater => 1,
+        Ordering::Equal => 0
+    }
+}
+
+// qsort。型安全にする為、ラッパーを書く.
+fn c_qsort<T: Ord>(data: &mut [T]) {
+    // transmuteでキャスト.
+    let _ptr: extern fn(*const c_void, *const c_void) -> c_int
+        = unsafe { std::mem::transmute(_compare::<T> as (extern fn(*const T, *const T) -> c_int)) };
+    unsafe {
+        qsort(data.as_mut_ptr() as *mut c_void,
+              data.len() as size_t,
+              std::mem::size_of::<T>() as size_t,
+              _ptr);
+    }
+}
 
 // 計測マクロ.
 macro_rules! performance {
@@ -35,6 +69,10 @@ fn main() {
     performance!("Vec::sort  ", { _standard.sort(); });
     assert_eq!(_standard, r);
 
+    let mut _c_qsort = a.clone();
+    performance!("C qsort    ", { c_qsort(&mut _c_qsort); });
+    assert_eq!(_c_qsort, r);
+
     let mut _insert = a.clone();
     performance!("Insert Sort", { sort::insert::sort(&mut _insert); });
     assert_eq!(_insert, r);
@@ -52,6 +90,6 @@ fn main() {
     assert_eq!(_quick, r);
 
     let mut _bubble = a.clone();
-    performance!("Bubble Sort ", { sort::bubble::sort(&mut _bubble); });
+    performance!("Bubble Sort", { sort::bubble::sort(&mut _bubble); });
     assert_eq!(_bubble, r);
 }
