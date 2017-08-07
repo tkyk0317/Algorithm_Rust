25a3f4436d899e1fe1dd93de8968e79664edb743
takayuki tanaka <takayuki.1981.0317@gmail.com>
2017-08-07 11:33:22 +0900

    [add] 要素数を増加させながらアルゴリズムを検証できるよう、修正.

--- src/main.rs
+++ src/main.rs
@@ -54,46 +54,52 @@ macro_rules! performance {
 
 // main function.
 fn main() {
-    let mut a = vec![];
-    for _ in 0..10000 { a.push(rand::thread_rng().gen::<u16>()); }
-    //let a = vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-    let mut r = a.clone();
-    r.sort();
+    let mut count: i32 = 5000;
+    loop {
+        if count > 1000000 { break; }
 
-    println!("---------------------------------------");
-    println!("[sort] Item Count: {}", a.len());
-    println!("---------------------------------------");
+        let mut a = vec![];
+        for _ in 0..count { a.push(rand::thread_rng().gen::<u16>()); }
+        //let a = vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+        let mut r = a.clone();
+        r.sort();
 
-    // sort.
-    let mut _standard = a.clone();
-    performance!("Vec::sort  ", { _standard.sort(); });
-    assert_eq!(_standard, r);
+        println!("---------------------------------------");
+        println!("[sort] Item Count: {}", a.len());
+        println!("---------------------------------------");
 
-    let mut _c_qsort = a.clone();
-    performance!("C qsort    ", { c_qsort(&mut _c_qsort); });
-    assert_eq!(_c_qsort, r);
+        // sort.
+        let mut _standard = a.clone();
+        performance!("Vec::sort  ", { _standard.sort(); });
+        assert_eq!(_standard, r);
 
-    let mut _insert = a.clone();
-    performance!("Insert Sort", { sort::insert::sort(&mut _insert); });
-    assert_eq!(_insert, r);
+        let mut _c_qsort = a.clone();
+        performance!("C qsort    ", { c_qsort(&mut _c_qsort); });
+        assert_eq!(_c_qsort, r);
 
-    let mut _select = a.clone();
-    performance!("Select Sort", { sort::select::sort(&mut _select); });
-    assert_eq!(_select, r);
+        let mut _insert = a.clone();
+        performance!("Insert Sort", { sort::insert::sort(&mut _insert); });
+        assert_eq!(_insert, r);
 
-    let mut _heap = a.clone();
-    performance!("Heap Sort  ", { sort::heap::sort(&mut _heap); });
-    assert_eq!(_heap, r);
+        let mut _select = a.clone();
+        performance!("Select Sort", { sort::select::sort(&mut _select); });
+        assert_eq!(_select, r);
 
-    let mut _quick = a.clone();
-    performance!("Quick Sort ", { sort::quick::sort(&mut _quick); });
-    assert_eq!(_quick, r);
+        let mut _heap = a.clone();
+        performance!("Heap Sort  ", { sort::heap::sort(&mut _heap); });
+        assert_eq!(_heap, r);
 
-    let mut _bubble = a.clone();
-    performance!("Bubble Sort", { sort::bubble::sort(&mut _bubble); });
-    assert_eq!(_bubble, r);
+        let mut _quick = a.clone();
+        performance!("Quick Sort ", { sort::quick::sort(&mut _quick); });
+        assert_eq!(_quick, r);
 
-    let mut _merge = a.clone();
-    performance!("Merge Sort ", { sort::merge::sort(&mut _merge); });
-    assert_eq!(_merge, r);
+        let mut _bubble = a.clone();
+        performance!("Bubble Sort", { sort::bubble::sort(&mut _bubble); });
+        assert_eq!(_bubble, r);
+
+        let mut _merge = a.clone();
+        performance!("Merge Sort ", { sort::merge::sort(&mut _merge); });
+        assert_eq!(_merge, r);
+        count = count + 5000;
+    }
 }
